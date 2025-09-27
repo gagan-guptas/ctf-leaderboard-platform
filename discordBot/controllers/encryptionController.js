34a4d6cf22bb6350f4c5f472355d160daefba5b3
Gagan Gupta <gaganguptajjn@gmail.com>
2025-09-27 20:17:44 +0530

    delete discordBot

--- discordBot/controllers/encryptionController.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-exports.encrypt = async function(pass) {
-    return await bcrypt.hash(pass, 10);
-}
-
-exports.compare = async function(pass, hash) {
-    return await bcrypt.compare(pass, hash);
-}
